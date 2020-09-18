1a4dc198ee78bfcbe6ad5cd6989765186d895fa0
yehudaTiram <yehuda@atarimtr.co.il>
2020-09-18 10:37:30 +0300

    1

--- public/js/atr-posts-by-cat-public.js
+++ public/js/atr-posts-by-cat-public.js
@@ -1,45 +1,58 @@
-(function( $ ) {
-	'use strict';
+/** @format */
 
-	jQuery.fn.liScroll = function (settings) {
-		settings = jQuery.extend({
-			travelocity: 0.03
-		}, settings);
-		return this.each(function () {
-			var $strip = jQuery(this);
-			$strip.addClass("newsticker")
-			var stripHeight = 1;
-			$strip.find("li").each(function (i) {
-				stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
-			});
-			var $mask = $strip.wrap("<div class='mask'></div>");
-			var $tickercontainer = $strip.parent().wrap("<div class='tickercontainer'></div>");
-			var containerHeight = $strip.parent().parent().height();	//a.k.a. 'mask' width
-			$strip.height(stripHeight);
-			var totalTravel = stripHeight;
-			var defTiming = totalTravel / settings.travelocity;	// thanks to Scott Waye
-			function scrollnews(spazio, tempo) {
-				$strip.animate({ top: '-=' + spazio }, tempo, "linear", function () { $strip.css("top", containerHeight); scrollnews(totalTravel, defTiming); });
-			}
-			scrollnews(totalTravel, defTiming);
-			$strip.hover(function () {
-				jQuery(this).stop();
-			},
-				function () {
-					var offset = jQuery(this).offset();
-					var residualSpace = offset.top + stripHeight;
-					var residualTime = residualSpace / settings.travelocity;
-					scrollnews(residualSpace, residualTime);
-				});
-		});
-	};
-	$(document).ready(function () {
-		// Wait 2 secoonds before scroll
-		setTimeout(function() {
-			$("#ma-kore ul.atr-posts-by-cat-list").liScroll();
-		}, 2000);
-	});
+(function($) {
+  "use strict";
 
-
-})( jQuery );
-
+  jQuery.fn.liScroll = function(settings) {
+    settings = jQuery.extend(
+      {
+        travelocity: 0.03,
+      },
+      settings
+    );
+    return this.each(function() {
+      var $strip = jQuery(this);
+      $strip.addClass("newsticker");
+      var stripHeight = 1;
+      $strip.find("li").each(function(i) {
+        stripHeight += jQuery(this, i).outerHeight(true); // thanks to Michael Haszprunar and Fabien Volpi
+      });
+      var $mask = $strip.wrap("<div class='mask'></div>");
+      var $tickercontainer = $strip
+        .parent()
+        .wrap("<div class='tickercontainer'></div>");
+      var containerHeight = $strip
+        .parent()
+        .parent()
+        .height(); //a.k.a. 'mask' width
+      $strip.height(stripHeight);
+      var totalTravel = stripHeight;
+      var defTiming = totalTravel / settings.travelocity; // thanks to Scott Waye
+      function scrollnews(spazio, tempo) {
+        $strip.animate({ top: "-=" + spazio }, tempo, "linear", function() {
+          $strip.css("top", containerHeight);
+          scrollnews(totalTravel, defTiming);
+        });
+      }
+      scrollnews(totalTravel, defTiming);
+      $strip.hover(
+        function () {
+          console.log(jQuery(this));
+          jQuery(this).stop();
+        },
+        function() {
+          var offset = jQuery(this).offset();
+          var residualSpace = offset.top + stripHeight;
+          var residualTime = residualSpace / settings.travelocity;
+          scrollnews(residualSpace, residualTime);
+        }
+      );
+    });
+  };
+  $(document).ready(function() {
+    // Wait 2 secoonds before scroll
+    setTimeout(function() {
+      $("#ma-kore ul.atr-posts-by-cat-list").liScroll();
+    }, 2000);
+  });
+})(jQuery);
